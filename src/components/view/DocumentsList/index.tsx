0bf5eb8236588965902f4bce73dbb3c0d771c402
dreamcatcher0101 <dreamcatcher000101@gmail.com>
2023-08-01 03:35:44 +0900

    feat: add DocumentsList page

---
+++ src/components/view/DocumentsList/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const DocumentsListView: React.FC = () => {
+  return (
+    <div className="w-full h-full flex items-center justify-center">
+      <h1>Welcome to Documents List!!!</h1>
+    </div>
+  );
+};
---
+++ src/types/document.ts
@@ -0,0 +1,7 @@
+export type Document = {
+  id: number;
+  title: string;
+  body: string;
+  url: string;
+  labels?: string[];
+};
